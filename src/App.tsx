ce9a58a2fe34dfa738f58e5c2871c9e43221f48a
Pavle Milicevic <milicevic_pz@wvwc.edu>
2018-08-25 23:46:20 -0400

    fix: Replace depricated function
    StackNavigator is depricated so use createStackNavigator instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React, { PureComponent } from 'react';
-import { StackNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation';
 
 import DeviceSetup from './components/DeviceSetup';
 
-export const AppNavigator = StackNavigator(
+export const AppNavigator = createStackNavigator(
   {
     DeviceSetup
   },
